bdb4870b95228799407f2efc0d6d668cf6c23442
Chamika Jayarathna <failgames.lk@gmail.com>
2026-08-04 10:07:04 +0530

    🔧 chore: add shadcn dependency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: {
@@ -16,7 +20,7 @@ const RootLayout = ({
   children: React.ReactNode;
 }>) => {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" className={cn("h-full", "font-sans", geist.variable)}>
       <body className="min-h-full bg-background text-foreground font-sans antialiased">
         {children}
       </body>
